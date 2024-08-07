67da40dd24b162aa06583385a4716facd03a7dbf
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-07 10:58:27 +0530

    Update charbonnier_loss.py

--- losses/charbonnier_loss.py
+++ losses/charbonnier_loss.py
@@ -9,6 +9,7 @@ class CharbonnierLoss(torch.nn.Module):
         self.eps = 1e-6  # 论文超参数
 
     def forward(self, X, Y):
+        print(f"X shape: {X.shape}, Y shape: {Y.shape}")  # Add this line
         if X.size() != Y.size():
             Y = F.interpolate(Y, size=X.size()[2:], mode='bilinear', align_corners=False)
         diff = torch.add(X, -Y)
