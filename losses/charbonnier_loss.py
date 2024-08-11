4e6d03ace3e4231ef51ecac0bfecfe4f5666eff4
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-11 19:39:17 +0530

    Update charbonnier_loss.py

--- losses/charbonnier_loss.py
+++ losses/charbonnier_loss.py
@@ -9,10 +9,10 @@ class CharbonnierLoss(torch.nn.Module):
         self.eps = 1e-6  # 论文超参数
 
     def forward(self, X, Y):
-        print(f"X shape: {X.shape}, Y shape: {Y.shape}")  # Add this line
+          # Add this line
         if X.size() != Y.size():
             X = F.interpolate(X, size=Y.size()[2:], mode='bilinear', align_corners=False)
         diff = torch.add(X, -Y)
-        error = torch.sqrt(diff * diff + self.eps)
+        error = torch.sqrt((diff * diff) + (self.eps*self.eps))
         loss = torch.mean(error)  # 论文里面没有取均值，这里应该是为了数值小
         return loss
