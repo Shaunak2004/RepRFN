83896ff46cc465a5cbf89923dd56b7eb27c922b3
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-13 22:19:44 +0530

    Update psnr_ssim.py

--- basicsr/metrics/psnr_ssim.py
+++ basicsr/metrics/psnr_ssim.py
@@ -59,6 +59,31 @@ def calculate_psnr_pt(img, img2, crop_border, test_y_channel=False, **kwargs):
 
 
 @METRIC_REGISTRY.register()
+def ssim(img1, img2):
+    C1 = (0.01 * 255) ** 2
+    C2 = (0.03 * 255) ** 2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    # 根据参数构造一维高斯核
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    # 根据一维高斯核构造二维高斯核
+    window = np.outer(kernel, kernel.transpose())
+    # filter2D会默认填充图像使得输出尺寸和输入保持一致
+    # 我们需要的是没有涉及填充部分的输出，因此可以套用卷积尺寸公式out_size=input_size-kernel_size+1
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1 ** 2
+    mu2_sq = mu2 ** 2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+    
 def calculate_ssim(img1, img2, border=0):
     '''calculate SSIM
     the same outputs as MATLAB's
