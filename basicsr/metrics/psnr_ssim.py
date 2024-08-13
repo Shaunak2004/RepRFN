bca1aaa0d0efc9951443ace8a7b82607e9356fd1
Shaunak Mandal <141357103+Shaunak2004@users.noreply.github.com>
2024-08-13 22:04:04 +0530

    Update psnr_ssim.py

--- basicsr/metrics/psnr_ssim.py
+++ basicsr/metrics/psnr_ssim.py
@@ -9,43 +9,20 @@ from basicsr.utils.registry import METRIC_REGISTRY
 
 
 @METRIC_REGISTRY.register()
-def calculate_psnr(img, img2, crop_border, input_order='HWC', test_y_channel=False, **kwargs):
-    """Calculate PSNR (Peak Signal-to-Noise Ratio).
+def calculate_psnr(img1, img2, border=0):
+    # img1 and img2 have range [0, 255]
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    h, w = img1.shape[:2]
+    img1 = img1[border:h - border, border:w - border]
+    img2 = img2[border:h - border, border:w - border]
 
-    Reference: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
-
-    Args:
-        img (ndarray): Images with range [0, 255].
-        img2 (ndarray): Images with range [0, 255].
-        crop_border (int): Cropped pixels in each edge of an image. These pixels are not involved in the calculation.
-        input_order (str): Whether the input order is 'HWC' or 'CHW'. Default: 'HWC'.
-        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
-
-    Returns:
-        float: PSNR result.
-    """
-
-    assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
-    if input_order not in ['HWC', 'CHW']:
-        raise ValueError(f'Wrong input_order {input_order}. Supported input_orders are "HWC" and "CHW"')
-    img = reorder_image(img, input_order=input_order)
-    img2 = reorder_image(img2, input_order=input_order)
-
-    if crop_border != 0:
-        img = img[crop_border:-crop_border, crop_border:-crop_border, ...]
-        img2 = img2[crop_border:-crop_border, crop_border:-crop_border, ...]
-
-    if test_y_channel:
-        img = to_y_channel(img)
-        img2 = to_y_channel(img2)
-
-    img = img.astype(np.float64)
+    img1 = img1.astype(np.float64)
     img2 = img2.astype(np.float64)
-
-    mse = np.mean((img - img2)**2)
+    mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return float('inf')
-    return 10. * np.log10(255. * 255. / mse)
+    return 20 * math.log10(255.0 / math.sqrt(mse))
 
 
 @METRIC_REGISTRY.register()
